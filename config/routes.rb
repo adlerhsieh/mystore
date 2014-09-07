ea9d8d3fe6cf726dc9dceadf4ea33e53ac59cff5
Adler <nkj20932@hotmail.com>
2014-09-08 01:04:07 +0800

    add style to layout file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get "sign_in", to: "sessions#new", as: "sign_in"
   get "sign_up", to: "users#new", as: "sign_up"
-  get "sign_out", to: "sessions#destroy", as: "sign_out"
+  delete "sign_out", to: "sessions#destroy", as: "sign_out"
 
   namespace :admin do
     resources :products
