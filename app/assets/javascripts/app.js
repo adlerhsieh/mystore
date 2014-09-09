3c1b375948f534bcb67e07648ec1d3c24ae0504e
Adler <nkj20932@hotmail.com>
2014-09-10 01:18:05 +0800

    debug display angular form in orders list

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,19 @@
 var myStore = angular.module("myStore", []);
 
+myStore.controller("ordersController", ['$scope', function($scope){
+	$scope.orders_list = gon.orders;
+	$scope.all_products = gon.products;
+
+	$scope.search_product = function(key){
+		for (var i in $scope.all_products) {
+			if ($scope.all_products[i].id == key){
+				return $scope.all_products[i].title;
+			};
+		};
+	};
+}]);
+
+myStore.controller("listController", ['$scope', function($scope){
+	this.list = gon.products;
+	this.specific_product = gon.specific_product;
+}]);
