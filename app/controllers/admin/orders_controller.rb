a93cd016083e952e1a4e3b2191b4341be6e4f6e3
Adler <nkj20932@hotmail.com>
2014-09-10 00:48:28 +0800

    debugging order list from angular not working at all

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,23 @@ class Admin::OrdersController < ApplicationController
   def index
   	@orders = Order.all
   	@products = Product.all
+
+  	gon.orders = Order.all
+  	gon.products = Product.all
   end
+
+  def edit
+  	@order = Order.find(params[:id])
+  end
+
+  def update
+
+  end
+
+	private
+
+		def orders_params
+			params.require(:order).permit(:customer, :quantity, :description, :product_id, :status)
+		end
+
 end
